9131cb23f761132cb4d961b7304a9c4faaf1e11e
Sebastian Martinez <sebastinez@me.com>
2021-09-30 18:06:18 +0200

    WIP: Add reward CLI
    Signed-off-by: Sebastian Martinez <sebastinez@me.com>

---
+++ reward/src/lib.rs
@@ -0,0 +1,267 @@
+use anyhow::{anyhow, bail};
+use coins_bip32::path::DerivationPath;
+use colored::*;
+use console::Term;
+use dialoguer::{theme::ColorfulTheme, Select};
+use ethers::{
+    signers::{HDPath, Ledger, Signer},
+    types::H160,
+};
+use git2::{Oid, Repository};
+use std::{fmt::Debug, path::PathBuf};
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct Puzzle {
+    org: H160,
+    contributor: H160,
+    commit: String,
+    project: String,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct Proof {
+    org: H160,
+    contributor: H160,
+    commit: String,
+    project: String,
+    org_sig: String,
+}
+
+/// Retrieves all notes from repo.
+/// Lets user chose which of the commits to claim.
+/// Obtains the proof stored in the commit
+/// Creates the transaction object, signs it by the contributor
+/// And sends it to the NFT factory
+pub fn claim(options: Options) -> anyhow::Result<()> {
+    let repo_path = options
+        .repo
+        .ok_or_else(|| anyhow!(Error::ArgMissing("No repo path specified".into())))?;
+    let repo = match Repository::open(repo_path) {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to open repo {}", e),
+    };
+
+    let mut commits: Vec<Oid> = Vec::new();
+
+    for note in repo.notes(None)? {
+        commits.push(note.unwrap().1);
+    }
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .items(&commits)
+        .with_prompt("Claimable Commits")
+        .interact_on_opt(&Term::stderr())?;
+
+    let index = match selection {
+        Some(index) => index,
+        None => bail!("User did not select any commit"),
+    };
+    log::debug!("Selected commit: {:?}", commits[index]);
+
+    let t = repo.find_note(None, commits[index])?;
+    log::debug!("Selected note: {:?}", t.id());
+
+    let t = t.message().unwrap();
+
+    let msg: Proof = serde_json::from_str(t).unwrap();
+
+    log::debug!("Retrieved Puzzle: {:?}", msg);
+
+    Ok(())
+}
+
+/// Creates a revwalk over the git repo
+/// Starting from the head iterates over all commits backwards, filtering out the ones that already have contribution notes
+/// Printing out a summary of all the commits which have no rewards defined
+pub fn discover(options: Options) -> anyhow::Result<()> {
+    let repo_path = options
+        .repo
+        .ok_or_else(|| anyhow!(Error::ArgMissing("No repo path specified".into())))?;
+
+    let repo = match Repository::open(repo_path) {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to open repo {}", e),
+    };
+    let head = repo.head().unwrap();
+    let target = head.target().unwrap();
+
+    let mut walk = repo.revwalk().unwrap();
+    walk.push(target).unwrap();
+
+    let oids: Vec<Oid> = walk
+        .by_ref()
+        .filter(|s| {
+            let oid = s.as_ref().unwrap();
+            repo.find_note(None, *oid).is_err()
+        })
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+
+    println!("{}", "Commits without existing puzzles".bold());
+    for oid in oids {
+        let commit = format_commit(&repo, &oid)?;
+        println!("{} {}", commit.0, commit.1);
+    }
+    Ok(())
+}
+
+/// Opens the repo checks if the passed commit exists on the repo
+/// With the commit hash and other params,creates the message
+/// The message is getting signed with a Ledger HW or a keystore file.
+/// And stored as a git note on the specified commit
+pub async fn create(options: Options) -> anyhow::Result<()> {
+    // Creating message
+    // -------------------
+    let oid = options
+        .commit
+        .ok_or_else(|| anyhow!(Error::ArgMissing("No commit specified".into())))?;
+    let contributor = options
+        .contributor
+        .ok_or_else(|| anyhow!(Error::ArgMissing("No contributor address specified".into())))?;
+    let project = options
+        .project
+        .ok_or_else(|| anyhow!(Error::ArgMissing("No project id specified".into())))?;
+    let repo_path = options
+        .repo
+        .ok_or_else(|| anyhow!(Error::ArgMissing("No repo path specified".into())))?;
+
+    let repo = match Repository::open(repo_path) {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to open repo {}", e),
+    };
+    let commit = repo
+        .find_commit(oid)
+        .map_err(|_| anyhow!(Error::CommitNotExisting))?;
+
+    // Sign message
+    // -----------------------
+    if let Some(keypath) = &options.keystore {
+        let prompt = format!("{} Password: ", "??".cyan());
+        let password = rpassword::prompt_password_stdout(&prompt).unwrap();
+        let signer = ethers::signers::LocalWallet::decrypt_keystore(keypath, password)
+            .map_err(|_| anyhow!("keystore decryption failed"))?;
+
+        let msg = create_puzzle(signer, contributor, commit.id().to_string(), project).await?;
+
+        let repo_sig = repo.signature()?;
+        let note = repo.note(&repo_sig, &repo_sig, None, commit.id(), &msg, true)?;
+        log::debug!(
+            "note id {}\ncreated on commit {}\nwith content {}",
+            note,
+            commit.id(),
+            &msg
+        );
+
+        Ok(())
+    } else if let Some(path) = &options.ledger_hdpath {
+        let hdpath = path.derivation_string();
+        let signer = Ledger::new(HDPath::Other(hdpath), 1).await?;
+
+        let msg = create_puzzle(signer, contributor, commit.id().to_string(), project).await?;
+
+        let repo_sig = repo.signature()?;
+        let note = repo.note(&repo_sig, &repo_sig, None, commit.id(), &msg, true)?;
+        log::debug!(
+            "note id {}\ncreated on commit {}\nwith content {}",
+            note,
+            commit.id(),
+            &msg
+        );
+
+        Ok(())
+    } else {
+        Err(anyhow!(Error::ArgMissing(
+            "no wallet specified: either '--ledger-hdpath' or '--keystore' must be specified"
+                .into()
+        )))
+    }
+}
+
+fn format_commit(repo: &Repository, oid: &Oid) -> anyhow::Result<(String, String)> {
+    let commit = repo.find_commit(*oid)?;
+    let summary = commit
+        .summary()
+        .ok_or_else(|| anyhow!(Error::NotValidEncoding("commit summary".into())))?;
+    Ok((oid.to_string()[..7].into(), summary.into()))
+}
+
+async fn create_puzzle<S: Signer>(
+    signer: S,
+    contributor: H160,
+    commit: String,
+    project: String,
+) -> anyhow::Result<String> {
+
+    // Instantiate of puzzle
+    let puzzle = Puzzle {
+        org: signer.address(),
+        contributor,
+        commit: commit.to_owned(),
+        project: project.to_owned(),
+    };
+
+    // Signing of puzzle and creation of signature 
+    let puzzle_json = serde_json::to_string(&puzzle)?;
+    let sig = signer.sign_message(&puzzle_json).await;
+    let sig = sig.map_err(|_| anyhow!(Error::SignFailure))?;
+    sig.verify(puzzle_json.to_owned(), signer.address())?;
+
+    // Creation of proof json
+    serde_json::to_string(&Proof {
+        org: signer.address(),
+        contributor,
+        commit,
+        project,
+        org_sig: sig.to_string(),
+    }).map_err(|_| anyhow!(Error::SerializeFailure))
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    /// No wallet specified.
+    #[error("Missing Argument: {0}")]
+    ArgMissing(String),
+    /// Not able to retrieve block .
+    #[error("not able to retrieve block")]
+    NoBlock,
+    /// Not able to retrieve block hash .
+    #[error("not able to retrieve block hash")]
+    NoBlockHash,
+    /// Not able to retrieve commit.
+    #[error("not able to retrieve commit")]
+    CommitNotExisting,
+    /// Not able to sign message
+    #[error("not able to sign message")]
+    SignFailure,
+    /// Not able to sign message
+    #[error("not able to sign message")]
+    SerializeFailure,
+    /// Not valid commit summary
+    #[error("{0} not valid")]
+    NotValidEncoding(String),
+    /// ETH signature failed
+    #[error("eth signature failed")]
+    ETHSigFailed,
+    /// GPG signature failed
+    #[error("{0}")]
+    GPGSigFailed(String),
+}
+
+/// The options allowed to be provided to the CLI
+#[derive(Debug, Clone)]
+pub struct Options {
+    /// Address of org.
+    pub org: Option<H160>,
+    /// Address of contributor
+    pub contributor: Option<H160>,
+    /// Repo path
+    pub repo: Option<PathBuf>,
+    /// Project id.
+    pub project: Option<String>,
+    /// Account derivation path when using a Ledger hardware wallet.
+    pub ledger_hdpath: Option<DerivationPath>,
+    /// Keystore file containing encrypted private key (default: none).
+    pub keystore: Option<PathBuf>,
+    /// SHA1 Hash of commit to reward
+    pub commit: Option<Oid>,
+}
---
+++ reward/src/main.rs
@@ -0,0 +1,159 @@
+use anyhow::anyhow;
+use coins_bip32::path::DerivationPath;
+use ethers::types::H160;
+use git2::Oid;
+use radicle_tools::logger;
+use reward::{claim, create, discover, Options};
+use std::{env, io::Write, path::PathBuf, process};
+
+const USAGE: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/", "USAGE"));
+const NAME: &str = env!("CARGO_CRATE_NAME");
+
+enum Command {
+    Create { options: Options, verbose: bool },
+    Claim { options: Options, verbose: bool },
+    Discover { options: Options, verbose: bool },
+    Help,
+}
+
+fn parse_options() -> anyhow::Result<Command> {
+    use lexopt::prelude::*;
+
+    let mut parser = lexopt::Parser::from_env();
+    let mut org: Option<H160> = None;
+    let mut repo: Option<PathBuf> = None;
+    let mut contributor: Option<H160> = None;
+    let mut project: Option<String> = None;
+    let mut ledger_hdpath: Option<DerivationPath> = None;
+    let mut keystore: Option<PathBuf> = None;
+    let mut commit: Option<Oid> = None;
+    let mut verbose = false;
+
+    while let Some(arg) = parser.next()? {
+        match arg {
+            Long("claim") => {
+                return Ok(Command::Claim {
+                    options: Options {
+                        org,
+                        contributor,
+                        repo,
+                        project,
+                        ledger_hdpath,
+                        keystore,
+                        commit,
+                    },
+                    verbose,
+                });
+            }
+            Long("discover") => {
+                return Ok(Command::Discover {
+                    options: Options {
+                        org,
+                        contributor,
+                        repo,
+                        project,
+                        ledger_hdpath: None,
+                        keystore: None,
+                        commit: None,
+                    },
+                    verbose,
+                })
+            }
+            Long("create") => {
+                return Ok(Command::Create {
+                    options: Options {
+                        org,
+                        contributor,
+                        repo,
+                        project,
+                        ledger_hdpath,
+                        keystore,
+                        commit,
+                    },
+                    verbose,
+                })
+            }
+            Long("repo") => {
+                repo = Some(parser.value()?.parse()?);
+            }
+            Long("org") => {
+                org = Some(parser.value()?.parse()?);
+            }
+            Long("contributor") => {
+                contributor = Some(parser.value()?.parse()?);
+            }
+            Long("project") => {
+                project = Some(parser.value()?.parse()?);
+            }
+            Long("commit") => {
+                commit = Some(parser.value()?.parse()?);
+            }
+            Long("keystore") => {
+                keystore = Some(parser.value()?.parse()?);
+            }
+            Long("ledger-hdpath") => {
+                ledger_hdpath = Some(parser.value()?.parse()?);
+            }
+            Long("verbose") | Short('v') => {
+                verbose = true;
+            }
+            Long("help") => {
+                return Ok(Command::Help);
+            }
+            _ => {
+                return Err(anyhow!(arg.unexpected()));
+            }
+        }
+    }
+    Ok(Command::Help)
+}
+
+#[tokio::main]
+async fn main() {
+    logger::init(NAME).unwrap();
+    logger::set_level(log::Level::Error);
+
+    match execute().await {
+        Err(err) => {
+            if let Some(cause) = err.source() {
+                log::error!("Error: {} ({})", err, cause);
+            } else {
+                log::error!("Error: {}", err);
+            }
+            process::exit(1);
+        }
+        Ok(()) => {}
+    }
+}
+
+async fn execute() -> anyhow::Result<()> {
+    match parse_options()? {
+        Command::Help => {
+            std::io::stderr().write_all(USAGE)?;
+            Ok(())
+        }
+        Command::Claim { options, verbose } => {
+            set_debug_level(verbose);
+            claim(options)?;
+            Ok(())
+        }
+        Command::Create { options, verbose } => {
+            set_debug_level(verbose);
+            create(options).await?;
+            Ok(())
+        }
+        Command::Discover { options, verbose } => {
+            set_debug_level(verbose);
+            discover(options)?;
+            Ok(())
+        }
+    }
+}
+
+fn set_debug_level(verbose: bool) {
+    if verbose {
+        logger::set_level(log::Level::Debug);
+    } else {
+        logger::set_level(log::Level::Info);
+    }
+}
